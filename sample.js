277356715b5738b7ca8aedac0ea23618a77cf3fc
Melito <bmelito@mitre.org>
2018-05-31 10:10:00 -0400

    Adds toggleClass usage

--- sample.js
+++ sample.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   let currentColor = "white"
   let currentNumber = 0
 
-  $("button").click(function(event){
+  $("button").click(function(){
     console.log($('.myInput').val())
   })
 
@@ -14,25 +14,32 @@ $(document).ready(function(){
 
   //Toggle the background color of the "Blue" button
 
-  $("#white-button").click(function(event){
-  	if (currentColor === "white") {
-  		$(this).css({"background-color": "blue"})
-  		currentColor = "blue"
-  	} else {
-  		$(this).css({"background-color": "white"})
-  		currentColor = "white"
-  	}
+  $("#white-button").click(function(){
+  	
+   //  if (currentColor === "white") {
+  		
+   //    $("#white-button").css({"background-color": "blue"})
+  	// 	currentColor = "blue"
+
+  	// } else {
+  		
+   //    $("#white-button").css({"background-color": "white"})
+  	// 	currentColor = "white"
+
+  	// }
+    $(this).toggleClass("main")
+
   })
 
   //Reveal the hidden paragraph
 
-  $("#paragraph").hover(function(event){
+  $("#paragraph").hover(function(){
   	$("#hidden-paragraph").show()
   })
 
   //Increase the value of the number in the middle of the button
 
-  $("#number-button").click(function(event){
+  $("#number-button").click(function(){
   	currentNumber = currentNumber + 1
   	$(this).text(currentNumber) 
   })
